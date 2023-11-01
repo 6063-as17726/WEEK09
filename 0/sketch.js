941ecfe251dcc57bfd16b4447070a3c56c12a9d2
Ayushi Shah <as17726@nyu.edu>
2023-11-01 16:40:01 -0400

    sketch 0

--- 0/sketch.js
+++ 0/sketch.js
@@ -7,11 +7,13 @@ function setup() {
   nSlider.position(500,100); 
 }
 
-let gridSize = 16; 
+let gridSize = 4; 
 
 function draw() {
+  randomSeed(1010); 
   background(0); 
-  stroke(255); 
+  stroke(0);
+  noStroke();  
   let nscale = nSlider.value(); 
 
   for(let y = 0; y<height; y+=gridSize) {
@@ -19,13 +21,24 @@ function draw() {
     for (let x =0; i<width; x+=gridSize) {
       //let y = height * noise(i/nscale); 
       let fc = 255 * noise(x/nscale, y/nscale); 
-      fill(fc); 
+      let aColor; 
+      if (fc>200) { 
+        aColor = "forestgreen";
+      } else if (fc>150) {
+        aColor ="sandybrown" ; 
+      } else if (fc>120) {
+        aColor = "brown" ; 
+      } else {
+        aColor = "mediumblue"; 
+      }
+      fill(aColor); 
       rect(x, y, gridSize, gridSize); 
       //line(i, height/2, i, y); 
+    } 
+
   }
 }
   // let x = frameCount % width; 
   // let y = height * noise(x/nscale); 
   // fill(255); 
   // ellipse(x, y, 20, 20); 
-}
